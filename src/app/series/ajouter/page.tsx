332c9f3117ccd22fd8f19c12bef9a35f8bf5e696
Marin ROUX <15229104+pharmarin@users.noreply.github.com>
2024-04-09 20:30:27 +0200

    👌 IMPROVE: Hide add button if show is already added

--- src/app/series/ajouter/page.tsx
+++ src/app/series/ajouter/page.tsx
@@ -1,71 +1,12 @@
-"use client";
+import AddShowClient from "@/app/series/ajouter/client";
+import { db } from "@/server/db";
 
-import { searchShow, upsertShow } from "@/app/series/ajouter/actions";
-import LoadingButton from "@/components/loading-button";
-import { Input } from "@/components/ui/input";
-import { PlusIcon } from "lucide-react";
-import { useAction } from "next-safe-action/hooks";
-import Image from "next/image";
+const AddShowPage = async () => {
+  const shows = await db.show.findMany({
+    select: { id: true },
+  });
 
-const AddShowPage = () => {
-  const { execute, result } = useAction(searchShow);
-
-  return (
-    <div className="space-y-4">
-      <form
-        action={execute}
-        className="flex w-full flex-row items-center justify-center gap-4"
-      >
-        <Input
-          className="text-[16px]"
-          name="query"
-          placeholder="Rechercher une série"
-          type="search"
-        />
-        <LoadingButton>Chercher</LoadingButton>
-      </form>
-      <div className="space-y-4 max-w-3xl">
-        {result.data?.map((result) => {
-          if (!result.id) {
-            return null;
-          }
-
-          const upsertShowWithId = upsertShow.bind(null, { showId: result.id });
-
-          return (
-            <div
-              key={result.id}
-              className="flex flex-row gap-4 rounded-lg bg-white bg-opacity-30 p-4"
-            >
-              <Image
-                alt={`${result.name} poster`}
-                className="w-24 h-fit"
-                src={`https://image.tmdb.org/t/p/w500${result.poster_path}`}
-                height={500}
-                width={500}
-              />
-              <div className="flex-1 flex-col">
-                <div className="text-xl font-bold text-white flex items-start justify-between">
-                  <div>{result.name}</div>
-                  {result.id && (
-                    <form action={upsertShowWithId}>
-                      <LoadingButton>
-                        <PlusIcon className="inline sm:hidden" />
-                        <span className="hidden sm:inline">
-                          Ajouter à mes séries
-                        </span>
-                      </LoadingButton>
-                    </form>
-                  )}
-                </div>
-                <div className="text-purple-200">{result.overview}</div>
-              </div>
-            </div>
-          );
-        })}
-      </div>
-    </div>
-  );
+  return <AddShowClient showIds={shows.map((show) => show.id)} />;
 };
 
 export default AddShowPage;
